5025adff0ff4d58a040878a008328f43a6fc081d
[elio] <[castels@hotmail.it]>
2022-10-05 11:38:48 +0100

    adding jest npm

---
+++ config-jest.js
@@ -0,0 +1,8 @@
+//in questo file andremo ad creare un oggetto e si fa utilizzando module.export
+//esplicitimao che il nostro enviroment sara node
+//dobbiamo settare anche il verbose = true
+//in modo da vedere il nome di tutti i test che sono stati fatti
+module.exports = {
+    testEnviroment:"node",
+    verbose: true
+}
--- example.js
+++ example.js
@@ -1,5 +1,2 @@
-//in questa lezione vedremo come testare la nostra applicazione 
-//con un altro strumento chiamato POSTMAN
-//installiamo postman aprimo una nuova collection e la chiamiami json placeholder API
-//su postman sulla nostra API e inseriamo all interno della barra URL la nostra API
-//vedere direttamente l applicazione installata postman
+//in questa lezione andremo a avedere come testare la nostra API con jest 
+//per usare al meglio jest e meglio creare un config file che chiameremo config.jest.js => 
