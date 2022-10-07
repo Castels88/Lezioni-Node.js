01056e5cef6b837371d3f6898a68f46a6a3ee7a5
[elio] <[castels@hotmail.it]>
2022-10-07 15:41:39 +0100

    adding other package

--- example.js
+++ example.js
@@ -2,3 +2,4 @@
 //prima cosa andiamo ad installare l'estenzione editorconfig in Vs
 //dopo andiamo col mause nella aerea di lavoro e col testo destro  creaimo un
 // file editor config
+//creaimo anche un file prettier -> con questo comando echo {}> .prettierrc.json
--- jest-config.js
+++ jest-config.js
@@ -1,8 +1,8 @@
 //qui dentro andiamo a crare il nostro test enviorement
-//prossimo passaggio andare nel package json e modificare la stringa test 
-// dove non avremo nessun ritorno con jest 
-//e adesso siamo pronti anche a gestire i nostri test sul  server 
-module.exports={
-    testEnviroment:"node",
-    verbose:true
-}
+//prossimo passaggio andare nel package json e modificare la stringa test
+// dove non avremo nessun ritorno con jest
+//e adesso siamo pronti anche a gestire i nostri test sul  server
+module.exports = {
+    testEnviroment: "node",
+    verbose: true,
+};
