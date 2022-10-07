bef98baf61f519bb2618c23b8bdfe0a1bc4daf05
[elio] <[castels@hotmail.it]>
2022-10-07 10:26:32 +0100

    fine lezione

--- example.js
+++ example.js
@@ -1,15 +1,3 @@
-//in questa lezione vedremo come runnare i nostri script nel package json 
-//nel pchage json avremo nella sezione script test e una string 
-//se facciamo npm run 
-// $ npm run
-// Lifecycle scripts included in lezioni-node@1.0.0:
-//   test
-//     echo "Error: no test specified" && exit 1
-//in questa sezione script possiamo anche aggiungere 
-//la dichiarazione e la frase che vogliamo quando runniamo un test 
-//tipo posso aggiungere la sezione task e mettere la frase che voglio 
-//infatti se eseguo npm run task
-// $ npm run task
-
-// > lezioni-node@1.0.0 task
-// > Doing a task ....
+// in questa lezione andremo ad installare jest nel nostro progetto 
+// npm install --save-dev jest
+//una volta installato andiamo a creare il nostro file jest.config.js =>
