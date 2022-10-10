8be085341886e275fc623ae565c02c2f4ebd7d72
[elio] <[castels@hotmail.it]>
2022-10-10 10:58:20 +0100

    fine lezione

--- example.js
+++ example.js
@@ -1,4 +1,2 @@
-//in questa lezione vedremo come usare and installare eslint
-//ci permette di trovare e fixare problemi nel nostro codice javascript
-//come prima cosa installiamo il pacchetto => $ npm install --save-dev eslint eslint-config-prettier
-//creaimo il file eslint.js in cui creeremo il nostro eslint compiler
+//in questa lezione vedremo come installalre jest e utilizzarla in typescrit
+//vedere il video
