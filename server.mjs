319555b5104af05aa08530a888ffdc6379486514
[elio] <[castels@hotmail.it]>
2022-10-11 12:08:52 +0100

    adding express module

---
+++ server.mjs
@@ -0,0 +1,6 @@
+import express from "express";
+//in questa lezione vedremo come creare callback tramite
+//il metodo middleware
+//come prima cosa andiamo ad utilizzare l express metodo per iniziare la nostra app server
+
+const app = express();
