8f4bfeafb1d7100e2c7395545b267824f9689166
[elio] <[castels@hotmail.it]>
2022-10-11 13:29:11 +0100

    fine lezione

--- server.mjs
+++ server.mjs
@@ -5,7 +5,12 @@
 import express from "express";
 
 const app = express();
-
+//come prima cosa dobbiamo usare il metodo use al quale all interno andremo ad assegnare
+// una funzione per gestire header del nostro server,
+//come in ogni creazione di server servono gli argomenti request => per richiedere i dati
+// e response=> per avere un responso dei dati raccolti
+//in middleware alcosa particolare e che si deve mettere la keyword next come argomento e richiamarla alla
+//fine per far si che midddleware funzioni
 app.use(function setHeaderMiddleware(request, response, next) {
   console.log("called: setHeaderMiddleware");
 
@@ -13,6 +18,9 @@ app.use(function setHeaderMiddleware(request, response, next) {
 
   next();
 });
+//con questa stringa di codice possiamo andare a setare il caso il nostro errore
+//quindi aggiungendo al next il new error possiamo settare l-eventuale errore che dovra
+//darci il nostro server
 
 // app.use(function middlewareWithAnError(request, response, next) {
 //   console.log("called: middlewareWithAnError");
@@ -20,22 +28,23 @@ app.use(function setHeaderMiddleware(request, response, next) {
 //   next(new Error("There has been an error."));
 // });
 
-// app.use(function sendDataMiddleware(request, response, next) {
-//   console.log("called: sendDataMiddleware");
+//questo andra a manadare i dati al nostro server
+app.use(function sendDataMiddleware(request, response, next) {
+  console.log("called: sendDataMiddleware");
 
-//   response.json({ data: "This is some data" });
+  response.json({ data: "This is some data" });
 
-//   next();
-// });
+  next();
+});
 
-// app.use(function errorHandlerMiddleware(error, request, response, next) {
-//   console.log("called: errorHandlerMiddleware");
+app.use(function errorHandlerMiddleware(error, request, response, next) {
+  console.log("called: errorHandlerMiddleware");
 
-//   response.status(500);
+  response.status(500);
 
-//   response.json({ message: error.message });
+  response.json({ message: error.message });
 
-//   next();
-// });
+  next();
+});
 
-// app.listen(3000);
+app.listen(3000);
