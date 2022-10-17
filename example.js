891af43efe73c5412ad605c35e72e38efb2c1166
[elio] <[castels@hotmail.it]>
2022-10-17 15:28:18 +0100

    fine lezione

--- example.js
+++ example.js
@@ -1,4 +1,5 @@
-//in questa lezione vedremo come aggiungere una nuova Route alla nostra API come prima cosa per rendere
-//il nostro server fattibile ad un test creaimo una nuova app.ts nella cartella src poi tagliamo ed incolliamo il nostro server.ts fino
-// Route ed espotiamo il file app e lo importiamo in server
-//andiamo a testare tutto con super test quindi prima installiamo super test
+//in questa lezione vedremo come utilizzare le variabili di condizione e come usarle
+//possono essere utilizzate per causare effetti sui processi del computer
+//se scriviamo nel terminale il comando env => otteremo una lista delle variabili di condizione che esistono
+//queste hanno delle chiavi e dei valori .
+//possiamo con node accedere a queste variabili con npm run dev e poi nel server.ts =>
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,19 @@
+import "dotenv/config";
+
 import app from "./app";
 
-const port = 3000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`)
 });
+//andiamo ad aggiungere un console.log
+// console.log(process.env);
+//noteremo che nel terminale spunteranno  tutte queste variabili
+//adesso andiamo a creare un file env in cui configureremo la nostra applicazione
+//in cui inseriremo la porta del nostro server e per leggere questo file dobbiamo installare un package dotenv
+//facciamo ripartire il nostro server e importiamo dotenv e invece di mettere 3000 alla costante port
+//possiamo inserire la env
+//e poi andiamo a modificare il package.json in modo da fargli guardare quando runniamo il server il .env file
+//aggiumgendo questa stringa "start:dev": "nodemon --watch dist/ --watch .env",
+//e adesso ogni volta che cambieremo la porta nel env verra cambiata anche la porta del nostro server
