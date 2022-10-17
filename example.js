89adfa407f1cf21497a8d5c9e40f7a7555d2c1e4
[elio] <[castels@hotmail.it]>
2022-10-17 13:29:22 +0100

    fix

--- example.js
+++ example.js
@@ -1,1 +1,4 @@
-//in questa lezione vedremo come ricompilare e far runnare automaticamemnte il nostro server
+//in questa lezione vedremo come aggiungere una nuova Route alla nostra API come prima cosa per rendere
+//il nostro server fattibile ad un test creaimo una nuova app.ts nella cartella src poi tagliamo ed incolliamo il nostro server.ts fino
+// Route ed espotiamo il file app e lo importiamo in server
+//andiamo a testare tutto con super test quindi prima installiamo super test
