d1bcbfd5025c8f405494622b7c4dff073cc25140
[elio] <[castels@hotmail.it]>
2022-10-17 18:09:50 +0100

    fine lezione

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,18 @@ import "express-async-errors";
 const app = express();
 
 app.get("/planets", (request, response) => {
-    response.json([
+    const planets = [
         {name: "Mercury"},
         {name: "Venus"}
-    ]);
+    ]
+
+    console.log({planets})
+
+    console.table({planets})
+
+    response.json(planets);
 });
 export default app
+//andiamo ad utilizzare uno di questi metodi nella nostra app come prima cosa avviamo la app
+//ogni volta che avvieremo il $ curl localhost:3005/planets -v nel nostro terminale verrano direttamente inseriti i risultati dei console
+
