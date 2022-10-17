329b3c2be45ee5a69b5f064480067c47a656d22a
[elio] <[castels@hotmail.it]>
2022-10-17 10:20:49 +0100

    creazione librerie

--- example.js
+++
@@ -1,7 +0,0 @@
-//in questa lezione vedremo come installare typescript per react
-// come prima cosa ninstalliamo il pacchetto typescrit
-// poi quello type@node
-//ora dobbiamo creare una configurazione per il tyscript comlipatore andiamo acreare un typeconfig => con un npm npm install --save-dev @tsconfig/node16
-//outDir sara la nostra cartella in cui salveremo
-//la successiva sono i file che vogliamo includere
-//l ultima i file che vogliamo escludere
