cbb0f32eba5b8d7b7e1bb1ff58f2393eb017facc
[elio] <[castels@hotmail.it]>
2022-10-18 15:21:34 +0100

    lezione interrotta manca postsql

---
+++ example.js
@@ -0,0 +1,3 @@
+//andiamo adesso ad installare prisma
+// per avviare prisma nel terminale si invia npx prisma init
+//una volta inviato questo comando verra creata una cartella prisma con un file schema.prisma
