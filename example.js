06f3feb55721706bf17abf000f6f91633241041b
[elio] <[castels@hotmail.it]>
2022-10-18 10:55:40 +0100

    fine lezione

--- example.js
+++ example.js
@@ -1,1 +1,4 @@
-//in questa lezione vedremo come usare il degub con VS
+//in questa lezione vedremo come usare i database types
+//database sono degli speciali files organizzati in una specifica maniera per organizzare dati
+//esistone 2 tipi di database sql database and relational database
+// guardare il video
